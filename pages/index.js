8d472e30197e92affb7f1b4c04b8116b487a073e
kprada <kprada@bicicletasmilan.com>
2025-03-05 07:24:27 -0500

    Cambios para preguntas

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,28 @@ import Head from 'next/head'
 import Layout from '../components/layout';
 import PDFList from '../components/pdf-list';
 import styles from '../styles/layout.module.css'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [PDFs, setPDFs] = useState([]);
+
+  useEffect(() => {
+    const fetchPDFs = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/pdfs`);
+        if (!response.ok) {
+          throw new Error('Error al obtener los PDFs');
+        }
+        const data = await response.json();
+        setPDFs(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchPDFs();
+  }, []);
+
   return (
     <div>
       <Head>
